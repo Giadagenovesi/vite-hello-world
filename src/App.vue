cf947b686180c54fb4cd576dae162b21ecae8491
Giadagenovesi <giada.g@outlook.it>
2023-04-12 16:54:15 +0200

    Completed Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
 import AppTitle from"./components/AppTitle.vue";
-
+import AppHero from"./components/AppHero.vue";
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -15,6 +16,7 @@ export default {
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 <style></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <img class="jumbotron-img" src="../assets/img/Superheroes+Squarespace+Banner+Image.png" alt="Superheroes">
+</template>
+
+<style>
+.jumbotron-img {
+    height: 400px;
+    width: 1000px;
+}
+</style>
